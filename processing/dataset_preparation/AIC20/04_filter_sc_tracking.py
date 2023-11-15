1f374bfcdc7cac92d9a8419b868f612876d53d6c
hector6298 <hectormrejia@gmail.com>
2023-11-14 21:30:48 -0500

    Add deduplication

--- processing/dataset_preparation/AIC20/04_filter_sc_tracking.py
+++ processing/dataset_preparation/AIC20/04_filter_sc_tracking.py
@@ -4,7 +4,10 @@ import sys
 sys.path.insert(1, osp.abspath('.'))
 import pandas as pd
 import json
-from modules.inference.postprocessing import filter_dets_outside_frame_bounds, remove_non_roi
+from modules.inference.preprocessing import (filter_dets_outside_frame_bounds, 
+                                             remove_duplicated_detections,
+                                             remove_non_roi)
+
 from modules.data_processor.utils import load_config
 
 # Loading configuration
@@ -31,8 +34,8 @@ if __name__ == "__main__":
         in_annotations = osp.join(sequence_path, camera_name, input_annotations_filename)
         out_annotations = osp.join(sequence_path, camera_name, out_annotations_filename)
         annotations_df = pd.read_csv(in_annotations)
-
-        print(f"Filtering SC track {in_annotations}")
+         
+        print(f"Filtering SC track {in_annotations}. Initial len {len(annotations_df)}")
 
         # Load logs from script 02_extract_frames to obtain frame dimensions
         with open(osp.join(logs_path, camera_name + '.json'), 'r') as camera_logs:
@@ -42,19 +45,23 @@ if __name__ == "__main__":
 
         # Filter detections that do not have a minimum area
         if min_bb_area:
-            annotations_df = annotations_df[(annotations_df['width'] * annotations_df['height']) >= 750]
-            print("Done filtering area")
+            annotations_df = annotations_df[(annotations_df['width'] * annotations_df['height']) >= min_bb_area]
+            print(f"Done filtering area. Len {len(annotations_df)}")
 
         # Remove detections that go out of frame boundaries
         if filter_frame_bounds:
             annotations_df = filter_dets_outside_frame_bounds(annotations_df, frame_width, frame_height)
-            print("Done filtering detections beyond boundaries")
+            print(f"Done filtering detections beyond boundaries. Len {len(annotations_df)}")
 
         # If required, remove detections outside region of interest
         if filter_roi:
             annotations_df = remove_non_roi(sequence_path_prefix, annotations_df)
-            print("Done RoI filtering of detections")
+            print(f"Done RoI filtering of detections. Len {len(annotations_df)}")
 
+        # Remove duplciated detections (grouped by id, frame, camera)
+        annotations_df = remove_duplicated_detections(annotations_df)
+        print(f"Done removing duplicated detections. Len {len(annotations_df)}")
+           
         # Save the filtered annotations
         annotations_df.to_csv(out_annotations)
         print(f"Saved processed annotation at {out_annotations}")
