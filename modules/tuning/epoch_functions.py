442de795ce8dba2ad14117709980959c7487bd6a
hector6298 <hectormrejia@gmail.com>
2023-12-05 05:17:06 +0100

    Update test to return all classification metrics

--- modules/tuning/epoch_functions.py
+++ modules/tuning/epoch_functions.py
@@ -3,7 +3,8 @@ import numpy as np
 from sklearn.metrics import (f1_score, 
                              accuracy_score, 
                              precision_score, 
-                             recall_score)
+                             recall_score,
+                             classification_report)
 
 def train_func(gnn_model, optimizer, train_loader, loss_fn, lr_scheduler=None):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -55,16 +56,20 @@ def test_func_multiclass(gnn_model, test_loader, loss_fn, task_type):
     all_targets = torch.cat(all_targets, dim=0)
     all_predictions = torch.cat(all_predictions, dim=0)
 
+    metrics_dict = classification_report(all_targets.cpu().numpy(), 
+                                         all_predictions.cpu().numpy(), 
+                                         output_dict=True)
     metrics = {
         "loss": loss_fn(all_logits, all_targets.to(device)).cpu().item(),
-        "macro_f1_score": f1_score(all_targets.cpu().numpy(), 
-                               all_predictions.cpu().numpy(), 
-                               average='macro'),
-        "accuracy": accuracy_score(all_targets.cpu().numpy(), 
-                                all_predictions.cpu().numpy()),
-        "precision": precision_score(all_targets.cpu().numpy(), 
-                                all_predictions.cpu().numpy()),
-        "recall": recall_score(all_targets.cpu().numpy(), 
-                            all_predictions.cpu().numpy())
+        "macro_f1_score": metrics_dict['macro avg']['f1-score'],
+        "precision": metrics_dict['macro avg']['precision'],
+        "recall": metrics_dict['macro avg']['recall'],
+        "f1_class0": metrics_dict['0']['f1-score'],
+        "precision_class0": metrics_dict['0']['precision'],
+        "recall_class0": metrics_dict['0']['recall'],
+        "f1_class1": metrics_dict['1']['f1-score'],
+        "precision_class1": metrics_dict['1']['precision'],
+        "recall_class1": metrics_dict['1']['recall'],
+        "accuracy": metrics_dict['accuracy']
         }
     return metrics, graph, latent_nodes_feats, latent_edge_feats
