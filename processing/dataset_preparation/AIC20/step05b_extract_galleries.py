a3e9f8d011b946e0ba8f22e6016986c04d528557
Laura Pedrouzo <laura.pedrouzo@gmail.com>
2024-05-07 12:02:13 +0200

    Add gallery generation for GT in test sequences in the same script.
    For each training sequence, the galleries are generated for the ground truth (annotations_filename in yml config file).
    For each test sequence, the galleries are generated for both the ground truth (annotations_filename in yml config file) plus the SCT files (test_annotations_filename in yml config file)

--- processing/dataset_preparation/AIC20/step05b_extract_galleries.py
+++ processing/dataset_preparation/AIC20/step05b_extract_galleries.py
@@ -15,7 +15,7 @@ from modules.data_processor.utils import load_config
 def main_extract_galleries(config_filepath:str="config/preprocessing.yml")->None:
     common_config, task_config = load_config(config_filepath, "05b_extract_galleries")
     
-    print("Extracting embeddings for training sequences")
+    print("Extracting ground truth gallery embeddings for training sequences")
     for sequence_name in task_config['train_sequences']:
         print(f"Working on {sequence_name}")
 
@@ -25,7 +25,17 @@ def main_extract_galleries(config_filepath:str="config/preprocessing.yml")->None
                                        device='cpu')
         emb_proc.generate_embedding_galleries_single_camera(task_config["frames_per_gallery"])
     
-    print("Extracting embeddings for testing sequences")
+    print("Extracting ground truth gallery embeddings for testing sequences")
+    for sequence_name in task_config['test_sequences']:
+        print(f"Working on {sequence_name}")
+
+        emb_proc = EmbeddingsProcessor(sequence_path=common_config['sequence_path'], 
+                                       sequence_name=sequence_name, 
+                                       annotations_filename=task_config['annotations_filename'],
+                                       device='cpu')
+        emb_proc.generate_embedding_galleries_single_camera(task_config["frames_per_gallery"])
+    
+    print("Extracting SCT gallery embeddings for testing sequences")
     for sequence_name in task_config['test_sequences']:
         print(f"Working on {sequence_name}")
         
