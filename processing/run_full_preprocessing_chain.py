e1360ce0b02c07a5b8f14d4a5cab97f867597063
Laura Pedrouzo <laura.pedrouzo@gmail.com>
2024-08-08 02:48:04 +0200

    Adapt script to be run with different options

--- processing/run_full_preprocessing_chain.py
+++ processing/run_full_preprocessing_chain.py
@@ -1,38 +1,58 @@
 import argparse
 import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+
 from dataset_preparation.AIC20.step01_prep_videos_annotations import main_prep_videos_annotations
 from dataset_preparation.AIC20.step02_extract_frames import main_extract_frames
 from dataset_preparation.AIC20.step03_preprocess_annotations import main_preprocess_annotations
 from dataset_preparation.AIC20.step04_filter_sc_tracking import main_filter_sc_tracking
 from dataset_preparation.AIC20.step05_extract_reid_embeddings import main_extract_reid_embeddings
 from dataset_preparation.AIC20.step05b_extract_galleries import main_extract_galleries
+from tuning.step06_hyperparameter_tuning import main_training_hp_tuning
 
-def run_full_chain(config01:str, config02:str, config03:str, run_filters:bool, config04GT:str, config04SCT:str, config05GT:str, config05SCT:str, config05galleries:str):
-    # step 01:
-    main_prep_videos_annotations(config01)
-    # step 02
-    main_extract_frames(config02)
-    # step 03
-    main_preprocess_annotations(config03)
-    
-    if run_filters:
-        print("Running filters on GT and SCT annotation data")
-        # step 04 GT
-        main_filter_sc_tracking(config04GT)
-        # step 04 SCT
-        main_filter_sc_tracking(config04SCT)
-    else:
-        print("No filtering is done to annotations!")
-    
-    # step 05 
-    print("Extracting ReID embeddings for ground truth files")
-    main_extract_reid_embeddings(config05GT)
-    print("Extracting ReID embeddings for single-camera tracking files")
-    main_extract_reid_embeddings(config05SCT)
+def run_full_chain(args:argparse.Namespace):
+    """Runs the selected steps from the full chain: preprocessing + training
 
-    # step 05b - galleries
-    print("Extracting galleries")
-    main_extract_galleries(config05galleries)
+    Args:
+        args (argparse.Namespace): Contains the tasks selected to be run and the path to the configuration file
+    """
+    config_file = args.config_file
+    all_steps = args.all
+    preprocessing = args.preprocessing
+    training = args.training
+    s1 = args.s1
+    s3 = args.s3
+    s4 = args.s4
+    s5 = args.s5
+    s5b = args.s5b
+
+    if all_steps or preprocessing or s1:
+        # step 01:
+        main_prep_videos_annotations(config_file)
+        # step 02
+        main_extract_frames(config_file)
+    
+    if all_steps or preprocessing or s3:
+        # step 03
+        main_preprocess_annotations(config_file)
+    
+    if all_steps or preprocessing or s4:
+        #step 04  
+        main_filter_sc_tracking(config_file)
+
+    if all_steps or preprocessing or s5:
+        # step 05
+        main_extract_reid_embeddings(config_file)
+    
+    if all_steps or preprocessing or s5b:
+        # step 05b - galleries
+        main_extract_galleries(config_file)
+    
+    if all_steps or training:
+        # step 06 HP tuning and training
+        main_training_hp_tuning(config_file) 
         
      
      
@@ -40,34 +60,24 @@ def run_full_chain(config01:str, config02:str, config03:str, run_filters:bool, c
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
                     prog='MCMT GNN',
-                    description='Multi-Camera Multi-Object Tracking using Graph Neural Networks. Runs all preprocessing steps prior to training.',
+                    description='Multi-Camera Multi-Object Tracking using Graph Neural Networks.',
                     epilog='2024',
                     formatter_class=argparse.ArgumentDefaultsHelpFormatter
                     )
-    parser.add_argument("-s1", "--step1-config", metavar="01", default=os.path.join("..", "config", "preprocessing.yml"), help="Path to yml configuration file for step 1")
-    parser.add_argument("-s2", "--step2-config", metavar="02", default=os.path.join("..", "config", "preprocessing.yml"), help="Path to yml configuration file for step 2")
-    parser.add_argument("-s3", "--step3-config", metavar="03", default=os.path.join("..", "config", "preprocessing.yml"), help="Path to yml configuration file for step 3")
-    parser.add_argument("--no-filter", action="store_false", help="Add this flag to not run step 04 related to bounding box filtering")
-    parser.add_argument("-s4GT", "--step4GT-config", metavar="04GT", default=os.path.join("..", "config", "preprocessing.yml"), help="Path to yml configuration file for step 4 for GT files")
-    parser.add_argument("-s4SCT", "--step4SCT-config", metavar="04SCT", default=os.path.join("..", "config", "preprocessing.yml"), help="Path to yml configuration file for step 4 for SCT files")
-    parser.add_argument("-s5GT", "--step5GT-config", metavar="05GT", default=os.path.join("..", "config", "preprocessing.yml"), help="Path to yml configuration file for step 5 for GT files")
-    parser.add_argument("-s5SCT", "--step5SCT-config", metavar="05SCT", default=os.path.join("..", "config", "preprocessing.yml"), help="Path to yml configuration file for step 5 for SCT files")
-    parser.add_argument("-s5g", "--step5galleries-config", metavar="05gall", default=os.path.join("..", "config", "preprocessing.yml"), help="Path to yml configuration file for step 5b (galleries)")
+    parser.add_argument("-c", "--config-file", metavar="path", default=os.path.join("config", "configuration.yml"), help="Path to yml configuration file")
+    group = parser.add_argument_group("Task", "Select the task to run. Only one task from the following list can be selected.")
+    exgr = group.add_mutually_exclusive_group(required=True)
+    exgr.add_argument("--all", help="Runs all preprocessing steps and HP search / training", action="store_true")
+    exgr.add_argument("-p", "--preprocessing", help="Runs all preprocessing steps", action="store_true")
+    exgr.add_argument("-t", "--training", help="Runs HP search and training", action="store_true")
+    exgr.add_argument("-s1", help="Runs preprocessing steps 1 and 2", action="store_true")
+    exgr.add_argument("-s3", help="Runs preprocessing step 3", action="store_true")
+    exgr.add_argument("-s4", help="Runs preprocessing step 4", action="store_true")
+    exgr.add_argument("-s5", help="Runs preprocessing step 5", action="store_true")
+    exgr.add_argument("-s5b", help="Runs preprocessing step 5b", action="store_true")
 
 
+    args = parser.parse_args()   
 
-    args = parser.parse_args()
-
-    config01 = args.step1_config
-    config02 = args.step2_config
-    config03 = args.step3_config
-    run_filters = args.no_filter
-    config04GT = args.step4GT_config
-    config04SCT = args.step4SCT_config
-    config05GT = args.step5GT_config
-    config05SCT = args.step5SCT_config
-    config05galleries = args.step5galleries_config
-    
-    
     # Run full chain 
-    run_full_chain(config01, config02, config03, run_filters, config04GT, config04SCT, config05GT, config05SCT, config05galleries)
+    run_full_chain(args)
